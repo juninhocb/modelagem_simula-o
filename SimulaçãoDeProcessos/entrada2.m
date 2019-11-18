a79348bf56a852c6a785019c9662a634efa7decf
juninhocb <53801149+juninhocb@users.noreply.github.com>
2019-11-18 11:20:59 -0300

    Add files via upload

---
+++ SimulaçãoDeProcessos/entrada2.m
@@ -0,0 +1,14 @@
+function g = entrada2(tempo)
+
+if (tempo < 8)
+    g =0; 
+else
+    g = 0.01;
+    
+end
+
+% a função foi deslocada para linear no ponto de operação que escolhemos 
+% x1 = 0.0016
+% x2 = 0 
+% f = 0.1 
+% tempo = 8 segundos 
